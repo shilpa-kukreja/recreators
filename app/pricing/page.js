cb0549b890e5413f306e3227521cc40847fb0179
shilpa-kukreja <recreaters.services@gmail.com>
2026-09-02 17:20:04 +0530

    pricing

--- app/pricing/page.js
+++ app/pricing/page.js
@@ -1,12 +1,12 @@
 import { Faq2 } from "@/components/Faq";
 import PageBanner from "@/components/PageBanner";
-import { Priceing2 } from "@/components/Priceing";
+import { Priceing } from "@/components/Priceing";
 import RiddaLayout from "@/layout/RiddaLayout";
 const page = () => {
   return (
     <RiddaLayout>
       <PageBanner pageTitle="Pricing" pageName="Pricing table" />
-      <Priceing2 />
+      <Priceing />
       <Faq2 />
     </RiddaLayout>
   );
